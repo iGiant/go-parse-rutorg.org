f2494714e34cc9b2a03a08981c441d32414d91d1
iGiant <sergey.grishayev@gmail.com>
2019-04-13 19:20:13 +0400

    func GetFilmNames

--- getFilm.go
+++ getFilm.go
@@ -6,8 +6,16 @@ import (
 	"io/ioutil"
 	"net/http"
 )
+const (
+	filmFileName = "films.txt"
+	passedFileName = "passed.txt"
+)
 func getFilmNames(filename string) ([]string, error) {
-	//
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(content), "/r/n"), nil
 }
 
 func getSiteBody(url, proxy string) ([]byte, error) {
