c614dd2759629c6f2de18a8a1e0ab3116022f859
iGiant <sergey.grishayev@gmail.com>
2019-04-13 18:54:02 +0400

    Структура в первом приближении

---
+++ getFilm.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"strings"
+	"io/ioutil"
+	"net/http"
+)
+func getFilmNames(filename string) ([]string, error) {
+	//
+}
+
+func getSiteBody(url, proxy string) ([]byte, error) {
+	//
+}
+func parseSite(body []byte, film string) bool {
+	//
+}
+
+func main() {
+	//
+}
